2cd5409df14088df484aa9c2a0f844773072b28d
Walter Joy IV <waltoids@users.noreply.github.com>
2020-05-30 11:35:25 -0400

    completed demo clear function

--- assets/js/script_1.js
+++ assets/js/script_1.js
@@ -1,15 +1,17 @@
 const movieInput = $("placeholder");
 const searchBtn = $("placeholder");
 const clearBtn = $("placeholder");
-const movieContainer = $("placeholder");
-const movieSimilar = $("placeholder");
+const movieContainerEl = $("placeholder");
+const movieSimilarEl = $("placeholder");
 
 const APIKEY1 = "373213-schoolpr-R7IG77YE";
 const APIKEY2 = "77c3dd39demsh29a349456660016p1393dbjsn48ff6737daa5"
 
 
 function clear() {
-
+    [...el.childNodes].forEach(element => {
+        element.remove();
+    })
 };
 
 searchBtn.addEventListener("click", function(){
@@ -17,7 +19,7 @@ searchBtn.addEventListener("click", function(){
     const search = movieInput.value
 
     clear()
-
+  
 
     $.ajax({
         url: queryURL,
