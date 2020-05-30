f2db571acc8f882c114bb2f38f661436823b761a
Walter Joy IV <waltoids@users.noreply.github.com>
2020-05-30 13:54:51 -0400

    added variables constraints and urls for queries

--- assets/js/script_1.js
+++ assets/js/script_1.js
@@ -1,28 +1,30 @@
+/* initalized variables from html */
 const movieInput = $("placeholder");
 const searchBtn = $("placeholder");
 const clearBtn = $("placeholder");
 const movieContainerEl = $("placeholder");
 const movieSimilarEl = $("placeholder");
-
+/* tastedive api */
 const APIKEY1 = "373213-schoolpr-R7IG77YE";
-const APIKEY2 = "77c3dd39demsh29a349456660016p1393dbjsn48ff6737daa5"
+/* Utelly api */
+const APIKEY2 = "f93ad3a3d7mshd6950dec518d715p16a532jsn2fe1c1a90753"
+/*query urls */
+const tasteDiveURL = `https://tastedive.com/api/similar?${movieInput}&k=373213-schoolpr-R7IG77YE`
+const utellyURL = `https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=${movieInput}&country=us`
+/*utelly info */
+// "x-rapidapi-host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
+// "x-rapidapi-key": "f93ad3a3d7mshd6950dec518d715p16a532jsn2fe1c1a90753"
 
-
+/* function to clear the elements */
 function clear() {
     [...el.childNodes].forEach(element => {
         element.remove();
     })
 };
 
+
+/*search function */
 searchBtn.addEventListener("click", function(){
     event.preventDefault();
-    const search = movieInput.value
-
     clear()
-  
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(updatePage);
-})
+});
