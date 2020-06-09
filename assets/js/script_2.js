ffda09f680aadd56ee4f315a82cf28910f3ebe17
larry laconi <soverylarry@gmail.com>
2020-06-08 21:37:44 -0400

    updating script_2 and adding that to HTML

---
+++ assets/js/script_2.js
@@ -0,0 +1,25 @@
+const movieInp = document.getElementById("search-bar");
+const searchButn = document.getElementById("searchBtn");
+const searchInp = movieInp.value;
+searchButn.addEventListener("click", function(){
+    event.preventDefault();
+    console.log(this)
+    platform()
+  });
+function platform(){
+var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": `https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=${movieInp.value}&country=uk`,
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
+        "x-rapidapi-key": "f93ad3a3d7mshd6950dec518d715p16a532jsn2fe1c1a90753"
+    }
+}
+$.ajax(settings).done(function (response) {
+    console.log(response);
+    $("#platforms").append(response.results[0].locations[0].display_name);
+    $("#platforms").append(response.results[0].locations[1].display_name);
+    $("#platforms").append(response.results[0].locations[2].display_name);
+});}
