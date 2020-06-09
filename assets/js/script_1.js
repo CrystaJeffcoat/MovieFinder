1622b6eec63168514f743c2e442d5ee0d5984982
Walter Joy IV <waltoids@users.noreply.github.com>
2020-06-09 17:12:22 -0400

    moved variables to global and added data.plot to similar movies 1,2,3

--- assets/js/script_1.js
+++ assets/js/script_1.js
@@ -2,6 +2,22 @@
 const movieInput = document.getElementById("search-bar");
 const searchBtn = document.getElementById("searchBtn");
 const clearBtn = document.getElementById("clearBtn");
+const similarMovieList = document.getElementById("rel-movie-1");
+const similarMovieList1 = document.getElementById("rel-movie-2");
+const similarMovieList2 = document.getElementById("rel-movie-3");
+const mainPoster = document.getElementById("mposter");
+const mrating = document.getElementById("mrating");
+const movieTitle = document.getElementById("mtitle");
+const movieplot = document.getElementById("plot");
+const movie1Poster = document.getElementById("rel-poster-1");
+const movie1Rating = document.getElementById("rel-rating-1");
+const movie2Poster = document.getElementById("rel-poster-2");
+const movie2Rating = document.getElementById("rel-rating-2");
+const movie3Poster = document.getElementById("rel-poster-3");
+const movie3Rating = document.getElementById("rel-rating-3");
+const movie1Plot = document.getElementById("plot-similar-movie-1");
+const movie2Plot = document.getElementById("plot-similar-movie-2");
+const movie3Plot = document.getElementById("plot-similar-movie-3");
 
 /* tastedive api key */
 const APIKEY1 = "373213-schoolpr-R7IG77YE";
@@ -23,10 +39,7 @@ function fetchMovieData(movie){
      
 };
 /* function that grabs info of movies similar searched from taste dive as well as movie data from OMBd */
-function similarMovies(search) {
-    const similarMovieList = document.getElementById("rel-movie-1");
-    const similarMovieList1 = document.getElementById("rel-movie-2");
-    const similarMovieList2 = document.getElementById("rel-movie-3");
+function movies(search) {
 
     fetchMovieSimilarity(search).then((data) => {
         const movie1 = `${data.Similar.Results[0].Name}`;
@@ -38,50 +51,38 @@ function similarMovies(search) {
         similarMovieList2.textContent = movie3;
 
         fetchMovieData(search).then((data) => {
-            const mainPoster = document.getElementById("mposter");
-            const mrating = document.getElementById("mrating");
-            const movieTitle = document.getElementById("mtitle");
-            const movieplot = document.getElementById("plot");
 
-            mainPoster.setAttribute("src", data.Poster)
-            mrating.textContent = `Rated: ${data.Rated}`
-            mtitle.textContent = `${data.Title}`
-            movieplot.textContent = `${data.Plot}`
-        })
+            mainPoster.setAttribute("src", data.Poster);
+            mrating.textContent = `Rated: ${data.Rated}`;
+            movieTitle.textContent = `${data.Title}`;
+            movieplot.textContent = `${data.Plot}`;
+        });
 
         fetchMovieData(movie1).then((data) => {
-            const movie1Poster = document.getElementById("rel-poster-1");
-            const movie1Rating = document.getElementById("rel-rating-1");
 
-            movie1Poster.setAttribute("src", data.Poster)
-            movie1Rating.textContent = `Rated: ${data.Rated}`
-        })
+            movie1Poster.setAttribute("src", data.Poster);
+            movie1Rating.textContent = `Rated: ${data.Rated}`;
+            movie1Plot.textContent = `${data.Plot}`;
+        });
 
         fetchMovieData(movie2).then((data) => {
-            const movie2Poster = document.getElementById("rel-poster-2");
-            const movie2Rating = document.getElementById("rel-rating-2");
 
-            movie2Poster.setAttribute("src", data.Poster)
-            movie2Rating.textContent = `Rated: ${data.Rated}`
-        })
+            movie2Poster.setAttribute("src", data.Poster);
+            movie2Rating.textContent = `Rated: ${data.Rated}`;
+            movie2Plot.textContent = `${data.Plot}`;
+        });
 
         fetchMovieData(movie3).then((data) => {
-            const movie3Poster = document.getElementById("rel-poster-3");
-            const movie3Rating = document.getElementById("rel-rating-3");
 
-            movie3Poster.setAttribute("src", data.Poster)
-            movie3Rating.textContent = `Rated: ${data.Rated}`
-        })
-    })
+            movie3Poster.setAttribute("src", data.Poster);
+            movie3Rating.textContent = `Rated: ${data.Rated}`;
+            movie3Plot.textContent = `${data.Plot}`;
+        });
+    });
 
-}
+};
 
-// /* function to clear the elements */
-// function clear(el) {
-//     [...el.childNodes].forEach(element => {
-//         element.remove();
-//     })
-// };
+
 
 
 /*search function */
@@ -91,7 +92,7 @@ searchBtn.addEventListener("click", function(){
   console.log(this)
   const search = movieInput.value;
   
-  similarMovies(search);
+  movies(search);
 
 });
 
